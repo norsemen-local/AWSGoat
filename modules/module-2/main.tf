8aa918dbe53e87ec0a09b6c122e8b4e064db83bb
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2024-06-29 15:27:21 +0300

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -406,7 +406,7 @@ data "template_file" "task_definition_json" {
 
 
 resource "aws_ecs_service" "worker" {
-  name                              = "ecs_service_worker"
+  name                              = "ecs_service_worker_Goat"
   cluster                           = aws_ecs_cluster.cluster.id
   task_definition                   = aws_ecs_task_definition.task_definition.arn
   desired_count                     = 1
